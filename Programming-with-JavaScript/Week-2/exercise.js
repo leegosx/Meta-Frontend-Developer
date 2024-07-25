4f310b9b2bd626f3ec79e732ac214e1cd814f7ef
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2024-07-26 01:27:36 +0200

    feat(add): letterFinder

---
+++ Programming-with-JavaScript/Week-2/exercise.js
@@ -0,0 +1,13 @@
+// Task 1
+function letterFinder(word, match) {
+    for (var i = 0; i < word.length; ++i) {
+        if (word[i] == match) {
+            console.log(`Found the match ${match} at ${i}`)
+        } else {
+            console.log(`---No match found at ${i}`)
+        }
+    }
+
+}
+
+letterFinder("test", "t")
