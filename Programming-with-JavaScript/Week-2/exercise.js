5b80a944075c84e1009adccd44e89669e1b966be
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2024-07-28 00:05:24 +0200

    feat(add): arrays, objects, and error handling

--- Programming-with-JavaScript/Week-2/exercise.js
+++ Programming-with-JavaScript/Week-2/exercise.js
@@ -11,3 +11,64 @@ function letterFinder(word, match) {
 }
 
 letterFinder("test", "t")
+
+
+// Exercise: Creating arrays and objects
+var clothes = [];
+
+clothes.push("Jeans");
+clothes.push("T-Shirts");
+clothes.push("Sneakers");
+clothes.push("Caps");
+clothes.push("Sweatshirts");
+
+clothes.pop();
+console.log(clothes);
+
+
+var favCar = {};
+
+favCar.color = "Yellow";
+favCar.covertible = true;
+
+console.log(favCar);
+
+
+// Example: Adding properties and methods to an object
+var car = {};
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+car.turnTheKey = function() {
+    console.log("The engine is running")
+}
+car.lightsOn = function() {
+    console.log("The lights are on.")
+}
+console.log(car);
+car.turnTheKey();
+car.lightsOn()
+
+// Tells the type of datatype
+console.log(typeof(car));
+console.log(typeof(car.turnTheKey));
+
+
+
+// Exercise: Error Handling
+function addTwoNums(a, b) {
+    try {
+        if(typeof(a) != "number") {
+            throw new ReferenceError("the first argument is not a number.")
+        } else if(typeof(b) != "number") {
+            throw new ReferenceError("the second argument is not a number.")
+        } else {
+            console.log(a + b)
+        }
+    } catch(err) {
+        console.log(`Error! ${err}`)
+    }
+}
+
+addTwoNums(5, 5)
+console.log("It still works");
