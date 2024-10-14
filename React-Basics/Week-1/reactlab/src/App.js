b01c71e8cbe28d7618fc8d9c413adef88528038d
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2024-10-14 08:40:52 +0200

    feat(lab): add props handling to Heading component

---
+++ React-Basics/Week-1/reactlab/src/App.js
@@ -0,0 +1,14 @@
+import Heading from './Heading'
+
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="Bob" />
+      <Heading firstName="John" />
+      <Heading firstName="Don" />
+      <Heading fristName="Dean" />
+    </div>
+  );
+}
+
+export default App;
