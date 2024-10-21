7eee84eadea67540a5c73b71e41ea2aeb6bbd571
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2024-10-21 10:28:30 +0200

    feat(lab): add logo image to App component

---
+++ React-Basics/Week-3/DisplayingImages/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./assets/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={logo} alt="Logo of the website">
+      </img>
+    </div>
+  );
+}
+
+export default App;
